740f723231991c9ecb961bd87683e29253bc272e
durkesh-datasci <nikola.anuja@gmail.com>
2025-03-25 19:11:16 +0530

    Initial commit for N-Queens-solver

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nqueens-solver",
+    version="0.1.0",
+    author="Durkesh",
+    author_email="durkeshpanuja@gmail.com",
+    description="A Python library to solve the N-Queens problem",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/nqueens_solver",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
